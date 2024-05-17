1249fc88ea9f562de5c9eb80e79926106fd91009
BoKyung Shin <musk82155@gmail.com>
2024-05-17 16:42:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request # , jsonify
+from flask import Flask, render_template, request
 import json
 import pandas as pd
 
@@ -15,9 +15,9 @@ def index():
 @app.route('/analyze', methods=['POST'])
 def analyze():
     if request.method == 'POST':
-        text = request.form['text']
+        data = request.get_json()
+        text = data.get('text')
         
-        # 데이터 전처리
         df = pd.DataFrame(columns=["본문"])
         df = df._append({'본문': text}, ignore_index=True)
         sentence_list = spliter(df)
@@ -28,12 +28,9 @@ def analyze():
         ner_result = ner_organizer(df2)
         sa_result, sa_positive, sa_negative = sa_organizer(df2)
         
-        # return render_template('index.html', ner_result=ner_result, sa_result=sa_result)
-        # return jsonify({'ner_result': ner_result, 'sa_result': sa_result})
         return json.dumps({'ner_result': ner_result, 'sa_result': sa_result
                            , 'sa_positive': sa_positive, 'sa_negative': sa_negative}
                            , ensure_ascii = False)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    # app.run(debug=True) # getaddinfo failed 에러 발생 시 이 코드 사용
